28c2087bd61da38483f49fceba117048ab9f7828
Victor Hermes <me.victorhermes@gmail.com>
2020-05-13 11:03:07 -0300

    relacionamentos

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,9 +1,14 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    ManyToOne,
+    JoinColumn,
+} from 'typeorm';
 
-interface AppointmentConstructor {
-    provider: string;
-    date: Date;
-}
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -11,10 +16,20 @@ class Appointment {
     id: string;
 
     @Column()
-    provider: string;
+    provider_id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'provider_id' })
+    provider: User;
 
     @Column('timestamp with time zone')
     date: Date;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
 }
 
 export default Appointment;
