f3f7d74a3dfa18f872ac2d4f9daacc52ebb1e3a1
Victor Hermes <me.victorhermes@gmail.com>
2020-07-20 20:20:04 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 import uploadConfig from './config/upload';
+import cors from 'cors';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
